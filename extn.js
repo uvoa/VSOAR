86c5dc62cd2a02b9efde6ee2ced4176ff76012a1
uvoa <65998522+uvoa@users.noreply.github.com>
2020-08-04 12:47:35 -0400

    Update extn.js

--- extn.js
+++ extn.js
@@ -5,7 +5,7 @@ var mc = document.createElement('div');mc.id="MAIN_CONT";
 document.getElementsByTagName('body')[0].appendChild(mc);
 mc.innerHTML = `<div id="LEFT_PANEL"> <div></div>
 <div id="SECTION_SELECTOR"><h4 class="ON_OPEN">Open tabs</h4><h4>Archived</h4> <div id="HOME_BTN" onclick="focusHome()">
-<img src="RES/vecbitms/home.svg" alt="">
+<img src="https://uvoa.github.io/VSOAR/home.svg" alt="">
 </div></div>
 </div><div id="RIGHT_PANEL"></div>`;
 }
